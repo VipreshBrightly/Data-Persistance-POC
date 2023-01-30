f87448317abd021e7139cb83f5445a40503b42ac
VipreshBrightly <vipresh.pandey@brightlysoftware.com>
2023-01-30 11:44:06 +0530

    Added object Mappper and db connections

--- src/main/java/org/acme/HashMapConverter.java
+++ src/main/java/org/acme/HashMapConverter.java
@@ -2,6 +2,7 @@ package org.acme;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import javax.persistence.AttributeConverter;
 import java.io.IOException;
@@ -10,6 +11,7 @@ import java.util.Map;
 
 public class HashMapConverter implements AttributeConverter<Map<String, Object>, String> {
 
+    private final ObjectMapper objectMapper = new ObjectMapper();
     @Override
     public String convertToDatabaseColumn(Map<String, Object> customerInfo) {
 
@@ -28,8 +30,7 @@ public class HashMapConverter implements AttributeConverter<Map<String, Object>,
 
         Map<String, Object> customerInfo = null;
         try {
-            customerInfo = objectMapper.readValue(customerInfoJSON,
-                    new TypeReference<HashMap<String, Object>>() {});
+            customerInfo = objectMapper.readValue(customerInfoJSON, new TypeReference<HashMap<String, Object>>() {});
         } catch (final IOException e) {
             System.out.println("JSON reading error");
         }
