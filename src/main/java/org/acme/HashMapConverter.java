1f323ee3c4446f0dab64c8947fefaaab596913ac
VipreshBrightly <vipresh.pandey@brightlysoftware.com>
2023-01-30 11:15:47 +0530

    Added project

---
+++ src/main/java/org/acme/HashMapConverter.java
@@ -0,0 +1,39 @@
+package org.acme;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import javax.persistence.AttributeConverter;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapConverter implements AttributeConverter<Map<String, Object>, String> {
+
+    @Override
+    public String convertToDatabaseColumn(Map<String, Object> customerInfo) {
+
+        String customerInfoJson = null;
+        try {
+            customerInfoJson = objectMapper.writeValueAsString(customerInfo);
+        } catch (final JsonProcessingException e) {
+            System.out.println("JSON writing error");
+        }
+
+        return customerInfoJson;
+    }
+
+    @Override
+    public Map<String, Object> convertToEntityAttribute(String customerInfoJSON) {
+
+        Map<String, Object> customerInfo = null;
+        try {
+            customerInfo = objectMapper.readValue(customerInfoJSON,
+                    new TypeReference<HashMap<String, Object>>() {});
+        } catch (final IOException e) {
+            System.out.println("JSON reading error");
+        }
+
+        return customerInfo;
+    }
+}
